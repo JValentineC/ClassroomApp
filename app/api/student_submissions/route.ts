6e2c854f6c71760b3126b66d0a97797c162aef21
Jonathan <jramirez@icstars.org>
2025-03-11 19:14:51 -0500

    Amplifying the app for hosting

--- app/api/student_submissions/route.ts
+++ app/api/student_submissions/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextRequest, NextResponse } from 'next/server';
 import { getAssignmentSubmissions, getUserAssignmentSubmission } from '@/lib/query/classroom';
 
